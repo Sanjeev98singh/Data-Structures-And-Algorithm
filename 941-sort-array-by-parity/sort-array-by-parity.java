7941e7ca6b7de9e467490fd736039c6567175a0c
Sanjeev Singh Bhadouria <99323593+Sanjeev98singh@users.noreply.github.com>
2024-02-12 11:34:48 +0530

    Time: 1 ms (66.14%) | Memory: 44.6 MB (73.80%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -2,15 +2,18 @@ import java.util.Arrays;
 
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        return Arrays.stream(nums)
-                .boxed() 
-                .sorted((a, b) -> {
-                  
-                    if (a % 2 == b % 2) return a - b; 
-                    else if (a % 2 == 0) return -1; 
-                    else return 1; 
-                })
-                .mapToInt(i -> i) 
-                .toArray(); 
+            int[] arr=new int[nums.length];
+            int evenindex=0;
+            int oddindex=nums.length-1;
+            for(int i =0 ;i<nums.length;i++){
+                if(nums[i]%2==0){
+                    arr[evenindex]=nums[i];
+                    evenindex++;
+                }else{
+                    arr[oddindex]=nums[i];
+                    oddindex--;
+                }
+            }
+        return arr;
     }
 }
