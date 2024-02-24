529b9c2c2bcf4ecd2cec2acc9a951ab31b3690bd
Sanjeev Singh Bhadouria <99323593+Sanjeev98singh@users.noreply.github.com>
2024-02-24 20:10:29 +0530

    Time: 79 ms (39.71%) | Memory: 54.1 MB (6.40%) - LeetSync

--- 258-add-digits/add-digits.js
+++ 258-add-digits/add-digits.js
@@ -3,13 +3,9 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    while(num>=10){
-        let sum=0;
-        while(num>0){
-            sum+=num%10;
-            num=Math.floor(num/10);
-        }
-        num=sum;
-    }
-    return num;
+    if(num==0)
+    return 0;
+    if(num%9===0)
+    return 9;
+    return num%9;
 };
